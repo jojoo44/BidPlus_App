c4620f47c7eb9760d33c98b1a8640d12c86dedfa
Aryam203 <162834209+Aryam203@users.noreply.github.com>
2026-04-14 02:03:09 +0300

    updte aryam

--- lib/contractor_proposal_details_screen.dart
+++ lib/contractor_proposal_details_screen.dart
@@ -1,6 +1,5 @@
 // contractor_proposal_details_screen.dart
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../main.dart';
 
